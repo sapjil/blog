8be08a2dd84217d3b06989117da31a8158e5b9d0
sapjilmaster <sapjilmaster@gmail.com>
2023-11-07 22:53:36 +0900

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,19 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 
 import sitemap from '@astrojs/sitemap';
+import partytown from '@astrojs/partytown';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://sapjil.net/',
-  integrations: [mdx(), sitemap()],
+  integrations: [
+    mdx(),
+    sitemap(),
+    partytown({
+      // Adds dataLayer.push as a forwarding-event.
+      config: {
+        forward: ['dataLayer.push'],
+      },
+    }),
+  ],
 });
